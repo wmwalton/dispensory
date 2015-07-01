618d52435b64e891e475ef9ab9a73d7d17cae189
Mike Walton <wmwalton.pro@gmail.com>
2015-06-30 22:13:38 -0700

    Update to cdisp.rb for "Make Change" code test

--- cdisp.rb
+++ cdisp.rb
@@ -22,32 +22,78 @@
 # Dollars.Cents or various ugly alternatives
 # 123  123.4  123.45  1.0  #  Fail on -1 or non numeric
 
+# Explored gem 'Money' for in/out parsing, cleaner handling of decimal
+require 'money'
+   Money.use_i18n = false
+
+# optparse does nice little banner and feedback on how to pass args, etc..
+# Want to either run a little "Enter Amount" query or put "enter in cents"
+# can use verbose for debug flag.  Trollop is another opt parser considered.
+
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options] ARGS
+  Provide value in cents to dispense change in US currency
+  example = $10.00  is  1000  "
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+end.parse!
+
+# p options
+# p ARGV
+
+# Static Ordered Denom
+
 DENOMINATIONS = {
-  :Hundred => 10000,
-  :Twenty => 2000,
-  :Ten => 1000,
-  :Five => 500,
-  :One => 100,
-  :Quarter => 25,
-  :Dime => 10,
-  :Nickel =>  5,
-  :Penny =>  1,
+  :$100 => 10000,
+  :$20 => 2000,
+  :$10 => 1000,
+  :$5 => 500,
+  :$1 => 100,
+  :Quarters => 25,
+  :Dimes => 10,
+  :Nickels =>  5,
+  :Pennies =>  1,
 }
 
+# Used Money Gem to provide clear USD output
+#
+
+def decimate(number)
+  rounded = (number*100).to_i / 100.0
+  if number == rounded
+    usd = Money.new(number, "USD")
+    puts "$#{usd}:"
+    # printf "$#{usd}:"
+    # fails
+    # printf "%.02f\n", number
+  else
+    printf "%g\n", number
+  end
+end
+
 # IN : Cents no decimal place
-# Divide amount by value until can't 
 # Dispense minimum quantity of bills and coins
+#
+# Core algorhythm loops through DENOMINATIONS and 
+# Divide amount by value until can't then next DENOM
 #
 
 def make_change(amount, denominations = DENOMINATIONS)
   change = {}  
-  denominations.each{|coin, value|
+    denominations.each{|coin, value|
     num, amount = amount.divmod(value)
     change[coin] = num if num > 0
   }
-  print " "
+  # print " "
   change
 end
- 
-[213,1120,1122,166,6543,234678,4242,66,8838].each{|i|
- puts "%-4i: %-10s" % [ i, make_change(i)]
+
+[213,1120,1122,166,12112,6543,234678,4242,66,8838].each{|i|
+  decimate(i)
+  puts "%8s: %-10s" % [ i, make_change(i)]
+}
